37050634eb17860e57dbe9e2a0f46b52a4a07f44
Ivan00333 <it000309@gmail.com>
2025-05-04 19:55:41 +0300

    tests for db added

--- tests/test_mariadb.py
+++ tests/test_mariadb.py
@@ -2,12 +2,62 @@ import pytest
 from db.db import Db
 
 
+class TestDb:
 
-def test_create_user(connection):
-    db = Db(connection, "test_create_user")
-    user_data = db.create_user_data()
-    id = db.create_user(user_data)
-    user_form_db = db.get_user_from_db_by_id(user_id=id)
-    print(user_data)
-    db.check_created_userIn_db(user_form_db, user_data)
-    db.delete_user_by_id(id)
+    def test_create_user(self, connection):
+        db = Db(connection, "test_create_user")
+        user_data = db.create_user_data()
+        id = db.create_user_in_db(user_data)
+        user_form_db = db.get_user_from_db_by_id(user_id=id)
+        print(user_data)
+        db.check_created_user_in_db(user_form_db, user_data)
+
+    def test_update_user(self, connection):
+        db = Db(connection, "test_update_user")
+
+        data_for_update = {
+            "firstname": "New",
+            "lastname": "Name",
+            "email": "new.email@example.com",
+            "telephone": "1111"
+        }
+
+        user_data = db.create_user_data()
+        user_id = db.create_user_in_db(user_data)
+        db.update_user(user_id, data_for_update)
+        updated_data = db.get_user_from_db_by_id(user_id)
+        db.check_updated_user(update_data=data_for_update, data_from_db=updated_data)
+
+    def test_negative_update_user(self, connection):
+        db = Db(connection, "test_negative_update_user")
+
+        data_for_update = {
+            "firstname": "New",
+            "lastname": "Name"
+        }
+
+        user_id = 99999
+        row_count = db.update_user(user_id, data_for_update)
+        assert row_count == 0, f"Ожидали 0 обновлённых строк, но обновлено: {row_count}"
+
+    def test_delete_user(self, connection):
+        db = Db(connection, "test_delete_user")
+        rows = db.get_rows_from_db(1)
+
+        if rows:
+            user_data = rows[0]
+            user_id = user_data["customer_id"]
+            db.delete_user_by_id(user_id)
+        else:
+            user_data = db.create_user_data()
+            user_id = db.create_user_in_db(user_data)
+            db.delete_user_by_id(user_id)
+
+        row = db.get_user_from_db_by_id(user_id)
+        assert row is None, f"Запись c customer_id={user_id} есть в таблице"
+
+
+    def test_delete_user_negative(self, connection):
+        db = Db(connection, "test_delete_user_negative")
+        row_count = db.delete_user_by_id(99999999)
+        assert row_count == 0, "Ожидали, что будет удалено 0 строк"
