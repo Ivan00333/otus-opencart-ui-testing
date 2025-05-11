848067c1259bed7f40be01910507e4d7849ad84d
Ivan00333 <it000309@gmail.com>
2025-05-11 23:37:35 +0300

    docker-compose.yml added

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -7,7 +7,7 @@ from locators.locators import HomePageLocators
 class TestHomePage:
     @allure.title("Проверка элементов на домашней странице")
     @pytest.mark.parametrize("locator", HomePageLocators.locators_list)
-    def test_opencart_home_page_elements_visible(self, driver, locator):
-        page = HomePage(driver)
+    def test_opencart_home_page_elements_visible(self, driver, locator, logger):
+        page = HomePage(driver, logger)
         page.open_home_page()
         page.check_element_visible(locator)
